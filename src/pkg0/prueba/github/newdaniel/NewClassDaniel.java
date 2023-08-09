22d1893246d6d23d6c2c839efa3db4845abcf04f
Danialefar <enc_danielfarias@hotmail.com.ar>
2023-08-09 19:00:28 -0300

    modifDaniel
    modique metodo leer Daniel y cree paq...

---
+++ src/pkg0/prueba/github/newdaniel/NewClassDaniel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg0.prueba.github.newdaniel;
+
+/**
+ *
+ * @author vvted
+ */
+public class NewClassDaniel {
+    
+}
